03a62f126a48317beca7f1400c185bf5f1bf33b1
toeesamp <tommi.sampo@gmail.com>
2020-09-23 19:39:29 +0300

    Exercise 10.3: the reviewed repositories list

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+const RepositoryItem = ({ item }) => {
+    // console.log(item);
+    return (
+        <View>
+            <Text>Full Name: {item.fullName}</Text>
+            <Text>Description: {item.description}</Text>
+            <Text>Language: {item.language}</Text>
+            <Text>Stars: {item.stargazersCount}</Text>
+            <Text>Forks: {item.forksCount}</Text>
+            <Text>Reviews: {item.reviewCount}</Text>
+            <Text>Rating: {item.ratingAverage}</Text>
+        </View>
+    );
+};
+
+export default RepositoryItem;
